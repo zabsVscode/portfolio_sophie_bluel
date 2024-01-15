12c2a06b835a465394b9a9abbe9a5cd199361ef2
zabsVscode <anthonyguiselin62@gmail.com>
2024-01-15 14:46:57 +0100

    portfolio_sophie_bluel

--- Backend/controllers/categories.controller.js
+++ Backend/controllers/categories.controller.js
@@ -18,3 +18,27 @@ exports.create = async (req, res) => {
 	})
 	return res.status(201).json(category)
 }
+
+
+
+const content = document.querySelector(".gallery");
+let images;
+
+async function fetchFiles() {
+    try {
+        const response = await fetch("http://localhost:5678/api");
+        images = await response.json();
+        console.log(images);
+
+        for (let i = 0; i < images.length; i++) {
+            const imageElement = document.createElement('img');
+            imageElement.src = images[i].url;
+            content.appendChild(imageElement);
+        }
+    } catch (error) {
+        console.error("Erreur lors de la récupération des images :", error);
+    }
+}
+fetchFiles();
+
+  
