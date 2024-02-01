39e91a7316146043ba1475f7037f5b6019894261
zabsVscode <anthonyguiselin62@gmail.com>
2024-02-01 15:24:06 +0100

    portfolio_sophie_bluel

--- FrontEnd/assets/js/loginscript.js
+++ FrontEnd/assets/js/loginscript.js
@@ -1,28 +1,56 @@
-let form = document.querySelector("form")
-let baliseEmail = document.getElementById("email")
+let form = document.querySelector("form");
+let baliseEmail = document.getElementById("email");
+let balisePassword = document.getElementById("password");
 
-function verifierChamp(balise) {
-  if (balise.value === "") {
-    balise.classList.add("error")
-  } else {
-    balise.classList.remove("error")
-  }
-}
+//EMAIL
 
 function verifierEmail(balise) {
-  let emailRegExp = new RegExp("[a-z._-]+@[a-z._-]+\\.[a-z._-]+")
-  if (emailRegExp.test(balise.value)) {
-    balise.classList.remove("error")
+  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z]{2,}");
+  if (balise.value !== "" && !emailRegExp.test(balise.value)) {
+    balise.classList.add("error");
   } else {
-    balise.classList.add("error")
+    balise.classList.remove("error");
   }
 }
 
-form.addEventListener("submit", (event) =>{
-  event.preventDefault()
-  verifierChamp(baliseEmail)
-})
+baliseEmail.addEventListener("change", () => {
+  if (baliseEmail.value !== "") {
+    verifierEmail(baliseEmail);
+  }
+});
 
-baliseEmail.addEventListener("change", () =>{
-  verifierChamp(baliseEmail)
-})
+/////// PASSWORD
+
+function verifierPassword(balise) {
+  
+  let passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  if (balise.value !== "" && !passwordRegExp.test(balise.value)) {
+    balise.classList.add("error");
+  } else {
+    balise.classList.remove("error");
+  }
+}
+
+balisePassword.addEventListener("change", () => {
+  if (balisePassword.value !== "") {
+    verifierPassword(balisePassword);
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  verifierEmail(baliseEmail);
+  verifierPassword(balisePassword);
+  
+  // Empêcher la soumission du formulaire si les champs ne sont pas valides
+  if (baliseEmail.classList.contains("error") || balisePassword.classList.contains("error")) {
+    event.preventDefault();
+  }
+});
+
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  verifierEmail(baliseEmail);
+});
+
