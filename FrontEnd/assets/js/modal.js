23c2405e3c0a28012e35fd0c900b7c66edec164f
zabsVscode <anthonyguiselin62@gmail.com>
2024-02-02 16:11:11 +0100

    portfolio_sophie_bluel

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,41 +1,38 @@
-let modal = null
+let modal = null;
 
-
-/* function ouverture de la modal */
-
+/* Function d'ouverture de la modal */
 const openModal = function(e) {
-e.preventDefault();
-const target = document.querySelector(e.target.getAttribute('href'))
-target.style.display = null;
-target.removeAttribute('aria-hidden');
-target.setAttribute('aria-modal', 'true');
-modal = target;
-modal.addEventListener('click', closeModal);
-modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
-modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = 'block'; // Correction : Affiche la modal en définissant son style sur 'block'
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+    modal = target;
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
 }
 
-/* function fermeture de la modal */
-
+/* Function de fermeture de la modal */
 const closeModal = function (e) {
-if (modal === null) return;
-e.preventDefault();
-modal.style.display = null;
-modal.setAttribute('aria-hidden', 'true');
-target.removeAttribute('aria-modal');
-modal = target;
-modal.removeEventListener('click', closeModal);
-modal.querySelector('.js-close-modal').removeEventListener('click', closeModal);
-modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
-modal = null;
+    if (modal === null) return;
+    e.preventDefault();
+    modal.style.display = 'none'; // Correction : Cache la modal en définissant son style sur 'none'
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal'); // Correction : Utilise removeAttribute au lieu de setAttribute
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-close-modal').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+
+    window.location.href = 'index.html';
 }
 
 const stopPropagation = function (e) {
-e.stopPropagation();
+    e.stopPropagation();
 }
 
-/* focus des éléments (peut être le mettre au début */
-
+/* Focus des éléments */
 document.querySelectorAll('.js-modal').forEach(a => {
-a.addEventListener('click', openModal)
-})
+    a.addEventListener('click', openModal);
+});
